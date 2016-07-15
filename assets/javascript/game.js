698ac00a6988994769bbf9b72263d005fe177b25
Antonio Deleon <antoniodeleon@Antonios-MacBook-Air.local>
2016-07-15 14:36:47 -0400

    hw-3-content

---
+++ assets/javascript/game.js
@@ -0,0 +1,58 @@
+//  variables
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessesSoFar = [];
+
+//  choices
+var computerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+// Computer Guess
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(computerGuess);
+
+
+// input from keyboard
+document.onkeyup = function(event) {
+
+	//  User Input
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log(userGuess);
+
+
+	// Conditions
+	if (userGuess == computerGuess) { 
+		wins++;
+		alert("You won! Play again!");
+		reset();
+
+	} else if (guessesLeft == 1) {
+		losses++;
+		alert("You lost! Try again!");
+		reset();
+	} else {
+		guessesSoFar.push(userGuess);
+		guessesLeft--;
+	}
+
+		// displaying to HTML
+		var html = "<p>Wins: " +wins +
+			"</p>" +
+			"<p>Losses: " +
+			losses +
+			"</p>" +
+			"<p>Guesses Remaining: " +
+			guessesLeft + "</p>" + "<p> Letters Guessed: " + guessesSoFar + " " + "</p>";
+
+		// Placing the html into the game ID
+		document.querySelector('#game').innerHTML = html;
+
+// function created to reset after a round
+function reset() {
+	guessesSoFar.length = 0;
+	guessesLeft = 9;
+	computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	console.log(computerGuess);
+}
+
+}
